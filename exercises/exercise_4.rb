e52be1aa03433be2f0838c7ee1c550e4ccb3d2f6
Adel Rashed <adel_rashed@outlook.com>
2020-07-21 22:34:05 +0000

    added three more storesa and queried stores that sell mens apparel and those that sell women apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name:"Surrey", annual_revenue:224000,  mens_apparel:false, womens_apparel:true)
+Store.create(name:"Whistler", annual_revenue: 1900000 , mens_apparel:true, womens_apparel:false)
+Store.create(name:"Yaletown", annual_revenue: 430000,  mens_apparel:true, womens_apparel:true)
+
+@menstores= Store.where(mens_apparel:true)
+@womenstores= Store.where(womens_apparel:true)
+
+
+puts "Stores that sell men's apparel"
+@menstores.each do |store|
+  puts "Store name : #{store.name}"
+  puts "Annual Revenue: #{store.annual_revenue}"
+  puts "---------------------------------------------"
+end
+
+
+puts "Stores that sell womens's apparel"
+@womenstores.each do |store|
+  puts "Store name : #{store.name}"
+  puts "Annual Revenue: #{store.annual_revenue}"
+  puts "---------------------------------------------"
+end
