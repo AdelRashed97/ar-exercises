ac2f1cbcd4e80cbda4077abfaee18e934acbc663
Adel Rashed <adel_rashed@outlook.com>
2020-07-22 03:14:40 +0000

    completed task 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Input a store name"
+store_name = $stdin.gets.chomp
+store = Store.create()
+if (!store.valid?)
+   store.errors.messages.each_pair{|attribute,message| puts "Attribute: #{attribute} \n #{message.join(" ")}"}
+  
+  
+end
